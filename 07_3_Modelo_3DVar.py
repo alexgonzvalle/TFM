326d535638faee6d64d54b9903cb5640e2ed82c4
alexgonzvalle <alexgonzvalle@gmail.com>
2024-07-31 09:45:22 +0200

    INITIAL

--- 07_3_Modelo_3DVar.py
+++ 07_3_Modelo_3DVar.py
@@ -4,45 +4,37 @@ from scipy.optimize import minimize
 from sklearn.linear_model import LinearRegression
 from data import get_data
 from stats import stats
-import dask.array as da
 
 
 def cost_function(x_flat, x_b, B_inv, y, R_inv, H):
-    n_obs = len(x_b)
-    x = x_flat.reshape((n_obs, -1))
+    x = x_flat.reshape(x_b.shape)
 
-    x_b_dask = da.from_array(x_b, chunks=(1000, 3))
-    y_dask = da.from_array(y, chunks=(1000, 1))
-
-    cost = da.sum([
-        0.5 * da.dot((x[i, :] - x_b_dask[i, :]).T, da.dot(B_inv, (x[i, :] - x_b_dask[i, :]))) +
-        0.5 * da.dot((H(x[i, :]) - y_dask[i, :]).T, da.dot(R_inv, (H(x[i, :]) - y_dask[i, :])))
-        for i in range(n_obs)
-    ])
-    return cost.compute()
+    cost = 0
+    for i in range(len(x_b)):
+        x_i = x[i, :]
+        y_i = y[i, :]
+        term1 = 0.5 * np.dot((x_i - x_b[i, :]).T, np.dot(B_inv, (x_i - x_b[i, :])))
+        term2 = 0.5 * np.dot((H(x_i) - y_i).T, np.dot(R_inv, (H(x_i) - y_i)))
+        cost += term1 + term2
+    return cost
 
 
 def jacobian(x_flat, x_b, B_inv, y, R_inv, H):
     n_obs = len(x_b)
     x = x_flat.reshape((n_obs, -1))
-
-    x_b_dask = da.from_array(x_b, chunks=(1000, 3))
-    y_dask = da.from_array(y, chunks=(1000, 1))
-
-    jac = da.concatenate([
-        da.dot(B_inv, (x[i, :] - x_b_dask[i, :])) + da.dot(R_inv, (H(x[i, :]) - y_dask[i, :]))
-        for i in range(n_obs)
-    ])
-    return jac.compute().flatten()
+    jac = np.zeros_like(x)
+    for i in range(n_obs):
+        x_i = x[i, :]
+        y_i = y[i, :]
+        jac[i, :] = np.dot(B_inv, (x_i - x_b[i, :])) + np.dot(R_inv, (H(x_i) - y_i))
+    return jac.flatten()
 
 
 def H(x):
-    # Supongamos que x contiene Hs, Tp y Dir en ese orden
-    Hs_model = x[0]  # Ajusta esto según el modelo real
-    return np.array([Hs_model])
+    return np.array([x[0]])
 
 
-plot = True
+plot = False
 df_boya = pd.read_csv('boyas.csv')
 df_res = pd.read_csv('res.csv')
 
@@ -73,7 +65,6 @@ for nombre in df_boya['Nombre']:
 
     # Dibujar
     hs_max = 14  # int(max([boya.hs.max(), gow.hs.max(), copernicus.VHM0.max(), y_cal_gow.max(), y_cal_cop.max()])) + 1
-    tp_max = int(max([boya.tp.max(), gow.tp.max(), copernicus.VTPK.max()])) + 1
 
     x_plot = np.linspace(0, hs_max, 11)
 
@@ -91,13 +82,13 @@ for nombre in df_boya['Nombre']:
     modelo_regresion.fit(x[:, 0].reshape(-1, 1), y_cal_cop)
     y_cal_cop_plot = modelo_regresion.predict(x_plot.reshape(-1, 1))
 
-    title = f'Modelo Multi Lineal {nombre}: y=a*Hs + b*Tp + c*Dire'
+    title = f'Modelo 3DVar {nombre}'
     bias_gow, rmse_gow, pearson_gow, si_gow = stats(boya.dir.values, boya.hs.values, gow.dir.values, gow.hs.values, y_cal_gow, hs_max, y_gow_plot, y_cal_gow_plot,
-                                                    'GOW', title, c='purple', fname=f'plot/model/03_MultiLineal/{nombre}_multilineal_gow.png', plot=plot)
+                                                    'GOW', title, c='purple', fname=f'plot/model/03_3DVar/{nombre}_3dvar_gow.png', plot=plot)
 
-    title = f'Modelo Multi Lineal {nombre}: y=a*Hs + b*Tp + c*Dire'
+    title = f'Modelo 3DVar {nombre}'
     bias_cop, rmse_cop, pearson_cop, si_cop = stats(boya.dir.values, boya.hs.values, copernicus.VMDR.values, copernicus.VHM0.values, y_cal_cop, hs_max, y_cop_plot, y_cal_cop_plot,
-                                                    'Copernicus', title, c='orange', fname=f'plot/model/03_MultiLineal/{nombre}_multilineal_cop.png', plot=plot)
+                                                    'Copernicus', title, c='orange', fname=f'plot/model/03_3DVar/{nombre}_3dvar_cop.png', plot=plot)
 
-    df_res.loc[len(df_res.index)] = [nombre, 'Lineal', bias_gow, bias_cop, rmse_gow, rmse_cop, pearson_gow, pearson_cop, si_gow, si_cop]
+    df_res.loc[len(df_res.index)] = [nombre, '3DVar', bias_gow, bias_cop, rmse_gow, rmse_cop, pearson_gow, pearson_cop, si_gow, si_cop]
 df_res.to_csv('res.csv', index=False)
